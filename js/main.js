7b51fce284b6f5fa492fab36b5423be9d20648aa
Piero Marini <piero.marini21@gmail.com>
2018-02-08 19:24:33 -0500

    Fixed Texts, Added Radio Buttons

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function ShowInput(){
+    var input = document.getElementById('recojo-u-input')
+    input.style.display = 'block'
+}
+function HideInput(){
+    var input = document.getElementById('recojo-u-input')
+    input.style.display = 'none'
+}
